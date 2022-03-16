1e87c0b1e1e7f68d44c8a426bad8cd199ede2fc5
Satish Challa <phani7satish@gmail.com>
2022-03-16 22:59:44 +0530

    Add project files.

---
+++ SeleniumPOM/Pages/HomePage.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace SeleniumPOM.Pages
+{
+    public class HomePage
+    {
+        public HomePage()
+        {
+            PageFactory.InitElements(WebD.Driver, this);
+        }
+
+        [FindsBy(How = How.Name, Using = "Initial")]
+        public IWebElement TxtInitial { get; set; }
+
+        [FindsBy(How =How.Name,Using = "FirstName")]
+        public IWebElement TxtFirstName { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//input[@value='female']")]
+        public IWebElement RdGenger { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//input[@value='Save']")]
+        public IWebElement BtnSave { get; set; }
+
+        public void CaptureUserForm(string initial, string fName)
+        {
+            TxtInitial.SendKeys(initial);
+            TxtFirstName.SendKeys(fName);
+            RdGenger.Click();
+            BtnSave.Submit();
+        }
+    }
+}
